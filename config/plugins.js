beef02a1bcf6f1b5add8148c1bcdb548792ffac2
Nick Nijenhuis <nick@novana.nl>
2022-12-10 22:43:02 +0100

    add cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -2,15 +2,29 @@ module.exports = ({ env }) => ({
     "netlify-deployments": {
       enabled: true,
       config: {
-        accessToken: "<netlify-access-token>",
+        accessToken: env('NETLIFY_ACCESS_TOKEN'),
         sites: [
           {
-            name: 'Site 1',
-            id: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-            buildHook: "https://api.netlify.com/build_hooks/<hook_id>",
+            name: env('NETLIFY_NAME'),
+            id: env('NETLIFY_ID'),
+            buildHook: "https://api.netlify.com/build_hooks/" + env('NETLIFY_HOOK_ID'),
             branch: 'main' // optional
           }
         ]
+      },
+      upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
+        },
       },
     },
   });
